5babfeeb6c367c586561b1525f1641c0fd53b154
Jordi <55429631+jorbush@users.noreply.github.com>
2024-09-18 17:37:25 +0200

    init spring boot

---
+++ postrify-backend/src/main/java/com/postrify/postrifybackend/PostrifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.postrify.postrifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostrifyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostrifyBackendApplication.class, args);
+    }
+
+}
