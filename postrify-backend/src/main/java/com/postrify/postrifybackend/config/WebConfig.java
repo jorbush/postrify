d5969901a3c996a5bfb69ea4e4d3268bd4f35dbe
Jordi <55429631+jorbush@users.noreply.github.com>
2024-09-18 20:47:10 +0200

    add origin

--- postrify-backend/src/main/java/com/postrify/postrifybackend/config/WebConfig.java
+++ postrify-backend/src/main/java/com/postrify/postrifybackend/config/WebConfig.java
@@ -9,6 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://postrify.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
