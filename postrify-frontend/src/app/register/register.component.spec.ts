1c52a0a460b04526947c385ab8cdafe960954bec
Jordi <55429631+jorbush@users.noreply.github.com>
2024-09-20 10:35:55 +0200

    angular testing

--- postrify-frontend/src/app/register/register.component.spec.ts
+++ postrify-frontend/src/app/register/register.component.spec.ts
@@ -1,16 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegisterComponent } from './register.component';
+import { AuthService } from '../services/auth.service';
+import { of, throwError } from 'rxjs';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let authService: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['register']);
+
     await TestBed.configureTestingModule({
-      imports: [RegisterComponent],
+      imports: [FormsModule, RegisterComponent],
+      providers: [
+        { provide: AuthService, useValue: authServiceSpy },
+        provideHttpClientTesting(),
+      ],
     }).compileComponents();
 
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,5 +29,41 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call register on AuthService and handle success', () => {
+    authService.register.and.returnValue(
+      of({ message: 'User registered successfully!' }),
+    );
+
+    component.username = 'testuser';
+    component.email = 'testuser@example.com';
+    component.password = 'testpass';
+
+    component.onSubmit();
+
+    expect(authService.register).toHaveBeenCalledWith(
+      'testuser',
+      'testuser@example.com',
+      'testpass',
+    );
+  });
+
+  it('should call register on AuthService and handle error', () => {
+    authService.register.and.returnValue(
+      throwError(() => new Error('Error registering')),
+    );
+
+    component.username = 'testuser';
+    component.email = 'testuser@example.com';
+    component.password = 'testpass';
+
+    component.onSubmit();
+
+    expect(authService.register).toHaveBeenCalledWith(
+      'testuser',
+      'testuser@example.com',
+      'testpass',
+    );
   });
 });
